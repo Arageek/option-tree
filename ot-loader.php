4c1bd1097a4dc2a25dfa88c48b79472d02869f31
Nashwan Doaqan <nashwan.doaqan@gmail.com>
2016-12-01 12:01:21 +0300

    Fix PHP notice on the theme mode.

--- ot-loader.php
+++ ot-loader.php
@@ -323,10 +323,12 @@ if ( ! class_exists( 'OT_Loader' ) ) {
       } else {
         if ( true == OT_CHILD_THEME_MODE ) {
           $path = ltrim( end( @explode( get_stylesheet(), str_replace( '\\', '/', dirname( __FILE__ ) ) ) ), '/' );
+          $path = ltrim( end( $path ), '/' );
           define( 'OT_DIR', trailingslashit( trailingslashit( get_stylesheet_directory() ) . $path ) );
           define( 'OT_URL', trailingslashit( trailingslashit( get_stylesheet_directory_uri() ) . $path ) );
         } else {
           $path = ltrim( end( @explode( get_template(), str_replace( '\\', '/', dirname( __FILE__ ) ) ) ), '/' );
+          $path = ltrim( end( $path ), '/' );
           define( 'OT_DIR', trailingslashit( trailingslashit( get_template_directory() ) . $path ) );
           define( 'OT_URL', trailingslashit( trailingslashit( get_template_directory_uri() ) . $path ) );
         }
